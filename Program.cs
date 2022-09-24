52d61f0e67ce0151373b98acd03580246e751084
Nadja Morin <nadja.morin@desinata.se>
2022-09-24 16:53:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using StudentGrades;
+
+//Get percentage
+var gradeCalculator = new GradeCalculator();
+
+Console.WriteLine("Enter the percentage: ");
+var percentage = Convert.ToInt32(Console.ReadLine());
+
+var grade = gradeCalculator.GetGradeByPercentage(percentage);
+
+//Print grade
+Console.WriteLine($"Student Grade: {grade}");
